a954c5ddd9883dd32be06a5c14252bba309c95aa
Charles <adeakinyemi129@gmail.com>
2023-11-25 23:47:17 +0100

    category button

--- components/categories.tsx
+++ components/categories.tsx
@@ -1,8 +1,10 @@
 'use client'
 
 import { Category } from "@prisma/client";
+import  qs  from "query-string"
 
 import { cn } from "@/lib/utils";
+import { useRouter, useSearchParams } from "next/navigation";
 
 interface CategoriesProps {
     data: Category[];
@@ -11,13 +13,31 @@ interface CategoriesProps {
 export const Categories = ({
     data
 }: CategoriesProps) => {
+    //hooks
+    const router = useRouter()
+    const searchParams = useSearchParams()
+
+    const categoryId = searchParams.get('categoryId')
+
+    const onClick = (id: string | undefined) => {
+        const query = {categoryId: id};
+
+        const url = qs.stringifyUrl({
+            url: window.location.href,
+            query,
+        }, {skipNull: true})
+
+        router.push(url)
+    }
+
+
     return (
         <div className="w-full overflow-x-auto space-x-2 flex p-1">
-            <button className={cn("flex items-center text-center text-xs md:text-sm px-2 md:px-4 py-2 md:py-3 rounded-md bg-primary/10 hover:opacity-75 transition")}>
+            <button onClick={() => onClick(undefined)} className={cn("flex items-center text-center text-xs md:text-sm px-2 md:px-4 py-2 md:py-3 rounded-md bg-primary/10 hover:opacity-75 transition")}>
                 Newest
             </button>
             {data.map((item) => (
-                <button key={item.id} className={cn("flex items-center text-center text-xs md:text-sm px-2 md:px-4 py-2 md:py-3 rounded-md bg-primary/10 hover:opacity-75 transition")}>
+                <button onClick={() => onClick(item.id)} key={item.id} className={cn("flex items-center text-center text-xs md:text-sm px-2 md:px-4 py-2 md:py-3 rounded-md bg-primary/10 hover:opacity-75 transition")}>
                     {item.name}
                 </button>
             ))}
