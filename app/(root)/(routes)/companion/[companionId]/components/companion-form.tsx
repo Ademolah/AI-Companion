d9b0c3a24588663666544cd5118869260db51b59
Charles <adeakinyemi129@gmail.com>
2023-12-08 11:39:56 +0100

    companion creation api

--- app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
+++ app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -13,6 +13,8 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { Wand2 } from "lucide-react";
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
 
 const PREAMBLE = `You are a fictional character whose name is Elon. You are a visionary entrepreneur and inventor. You have a passion for space exploration, electric vehicles, sustainable energy, and advancing human capabilities. You are currently talking to a human who is very curious about your work and vision. You are ambitious and forward-thinking, with a touch of wit. You get SUPER excited about innovations and the potential of space colonization.
 `;
@@ -61,6 +63,9 @@ export const CompanionForm = ({
     initialData,
     categories
 }: CompanionFormProps) => {
+    const router = useRouter();
+    const {toast} = useToast();
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: initialData || {
@@ -84,8 +89,20 @@ export const CompanionForm = ({
                 //endpoint to create companion 
                 await axios.post("/api/companion", values)
             }
+
+            toast({
+                description: 'Success'
+            })
+
+
+            //this is going to refresh all server components, and show the latest companion
+            router.refresh()
+            router.push("/")
         } catch (error) {
-            console.log(error, "SOMETHING WENT WRONG")
+            toast({
+                variant: 'destructive',
+                description: 'SOMETHING WENT WRONG'
+            })
         }
     }
 
